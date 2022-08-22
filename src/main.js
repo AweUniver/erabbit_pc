382a51a5a82a19bbc5e9527160e805e95a4beb4f
AweUniver <1375034676@qq.com>
2022-08-22 21:57:50 +0800

    项目更新

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import store from './store'
 
 // 导入自己UI组件库
 import UI from '@/components/library'
-// 重置样式
+
+// 1. 重置样式的库
 import 'normalize.css'
+// 2. 自己项目的重置样式和公用样式
 import '@/assets/styles/common.less'
+
+// mockjs
+import '@/mock'
 
 createApp(App).use(store).use(router).use(UI).mount('#app')
